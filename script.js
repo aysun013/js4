8ce031c21009d53f5e3e345de1c3316dcd340870
Aysun Ismayilova <147844360+aysun013@users.noreply.github.com>
2023-12-04 16:45:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+task1
+
+
+// let text = prompt(" bir metn daxil edin: ");
+// let text = document.getElementById('inp')
+
+
+// let uzunluq = text.length;
+
+
+// alert(" metnin uzunluğu: " + uzunluq);
+
+
+
+
+// task2
+
+
+
+
+  
+
+// task3
+
+
+// function textLength(text) {
+ 
+//     if (text.length > 100) {
+    
+//       let cutText = text.slice(0, 100);
+  
+     
+//       cutText += '...';
+  
+//       return cutText;
+//     }
+  
+   
+//     return text;
+//   }
+  
+//   let text1 = prompt(" bir cumle daxil edin: ");
+//   let text2 = textLength(text1);
+
+//   console.log(text2);
+
+
+
+
+
+function textLength({
+
+    let input = document.getElementById('inp').ariaValueMax;
+
+
+
+    if (input.length > 100){
+        div.innerHTML = input.substring(0,100)
+    }
+
+    else {
+        console.log('kicikdir')
+    }
+})
+  
